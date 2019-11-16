c59314da6dcca5f3e7e74ce60f2d1cb2cca5634a
Michael Eskarous <mneskarous@gmail.com>
2019-11-16 14:46:37 -0800

    Implemented map function for photos

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -5,9 +5,9 @@ class PhotoGrid extends React.Component {
   render() {
     return (
       <div className="photo-grid">
-        {this.props.posts.map((post, i) => (
-          <Photo {...this.props} key={i} index={i} post={post}/>
-        ))}
+        {this.props.posts.map((post, i) => {
+          return <Photo {...this.props} key={i} i={i} post={post}/>
+  })}
       </div>
     )
   }
