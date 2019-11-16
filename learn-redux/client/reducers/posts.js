500708de814675be84ea91c384ac366d50fa3bc2
Michael Eskarous <mneskarous@gmail.com>
2019-11-15 19:57:33 -0800

    Added console.log

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,8 +1,8 @@
 // a reducer takes in the action and the copy of the current state
 
 function posts(state = [], action) {
+  console.log("The post will change");
   console.log(state, action);
-
   return state;
 }
 
