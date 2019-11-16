1fcd2462b5054f6c2f94f71841e93d9c690d6685
Michael Eskarous <mneskarous@gmail.com>
2019-11-16 14:49:15 -0800

    Implemented increment function to onClick method

--- learn-redux/client/components/photo.js
+++ learn-redux/client/components/photo.js
@@ -30,7 +30,7 @@ class Photo extends React.Component {
         <figcaption>
           <p>{post.caption}</p>
           <div className="control-buttons">
-            <button onClick={this.props.increment.bind(null, i)} className="likes">&hearts; {post.likes}</button>
+            <button onClick={() => this.props.increment(i)} className="likes">&hearts; {post.likes}</button>
             <Link to={`/view/${post.code}`} className="button">
               <span className="comment-count">
                 <span className="speech-bubble"></span>
