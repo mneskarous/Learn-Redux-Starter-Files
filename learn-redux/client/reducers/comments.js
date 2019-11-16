27bdea00e7147d06a7eb088a06ab6008824bb9a9
Michael Eskarous <mneskarous@gmail.com>
2019-11-16 14:50:46 -0800

    Implemented comments and postComments reducers

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,8 +1,30 @@
 // a reducer takes in the action and the copy of the current state
 
-function comments(state = [], action) {
-  console.log(state, action);
+function postComments(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT': 
+      // return the new state with the new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }]
+    case 'REMOVE_COMMENT': 
+      console.log('Removing comment');
+      return state;
+    default: 
+     return state;
+  }
+}
 
+function comments(state = [], action) {
+  if (typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state, 
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
