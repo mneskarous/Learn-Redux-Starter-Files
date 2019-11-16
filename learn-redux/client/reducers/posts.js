d4968e3e4fd06612f98454edf914c45de3406430
Michael Eskarous <mneskarous@gmail.com>
2019-11-16 14:51:49 -0800

    Implemented posts reducer for incrementing likes

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,9 +1,19 @@
 // a reducer takes in the action and the copy of the current state
 
 function posts(state = [], action) {
-  console.log("The post will change");
-  console.log(state, action);
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES':
+      console.log('Incrementing likes');
+      const i = action.index;
+      console.log(action.index);
+      return [
+        ...state.slice(0, i),
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1)
+      ]
+    default: 
+      return state
+  }
 }
 
 export default posts;
